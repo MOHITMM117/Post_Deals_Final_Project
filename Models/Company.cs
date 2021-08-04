398299e486c4d3979fefcf9bb4345cb33b040342
Mohit Mohit <MM117@atcltd.atc.org.nz>
2021-08-05 09:27:12 +1200

    Add project files.

---
+++ Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Post_Deals_Final_Project.Models
+{
+    //Deal company details
+    public class Company
+    {
+        public int Id { get; set; } // company id 
+
+        public string CompanyName { get; set; } // comany name 
+
+        public string Email { get; set; } // email
+        
+        public string ContactNumber { get; set; } // contact number
+    }
+}
---
+++ Models/Deal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Post_Deals_Final_Project.Models
+{
+    //Deal information including the company deal heading and title
+    public class Deal
+    {
+        public int Id { get; set; } // deals id 
+
+        public string Heading { get; set; } // heading
+
+        public string DealInfo { get; set; } // infomation regarding deal
+
+
+        public int CompanyId { get; set; } // forign key 
+
+        public Company Company { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        
+
+
+
+    }
+}
